52b1a2cee230906402af6b6a085881f506b5f27d
sariket007 <94927046+sariket007@users.noreply.github.com>
2026-03-07 23:56:22 +0530

    Configure secure MongoDB connection

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
+require("dotenv").config();
 const express = require("express");
+const connectDB = require("./config/db");
+
+// Connect to Database
+connectDB();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
-  res.status(200).json({ message: "Financial Helper Server is running!" });
+  res.status(200).json({ message: "Financial Helper server is Running!" });
 });
 
 app.listen(PORT, () => {
