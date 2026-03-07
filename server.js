b5e876deb46b6cadb195b66dcd5d87e795ab801c
sariket007 <94927046+sariket007@users.noreply.github.com>
2026-03-07 19:27:32 +0530

    Error resolved for server running check and updated codes

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
+
 app.use(express.json());
 
 app.get("/api/health", (req, res) => {
-  res.status(200).json({ message: "Financial Helper server is Running!" });
+  res.status(200).json({ message: "Financial Helper Server is running!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server in running on port: ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
